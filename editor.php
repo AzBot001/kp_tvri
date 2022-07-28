6624a76d11f49b2810aea1aca95f544c1d6c44fa
AmirPaneo <amirrudinpaneo@gmail.com>
2022-07-29 06:13:29 +0800

    menambahkan data paket pada user editor

--- editor.php
+++ editor.php
@@ -5,19 +5,22 @@ include 'app/env.php';
 include 'base_url.php';
 if ($_SESSION['type_user'] != 'editor' || !isset($_SESSION['type_user'])) {
 
-    ?>
-        <script>
-            alert('Anda harus login untuk mengakses halaman ini!');
-            window.location.href = '<?= $base_url; ?>';
-        </script>
-    <?php
-        return false;
+?>
+    <script>
+        alert('Anda harus login untuk mengakses halaman ini!');
+        window.location.href = '<?= $base_url; ?>';
+    </script>
+<?php
+    return false;
 }
 
 if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'beranda_editor') {
     $title = 'Beranda';
     $icon = 'fas fa-dashboard';
 } else if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'dataBeritaNaskah_editor') {
+    $title = 'Data Naskah';
+    $icon = 'fas fa-edit';
+} else if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'buatpaket_editor') {
     $title = 'Data Naskah';
     $icon = 'fas fa-edit';
 }
@@ -29,9 +32,11 @@ include 'views/layout/sidebar.php';
 
 if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'beranda_editor') {
     include 'views/pages/editor/beranda_editor.php';
-}else if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'dataBeritaNaskah_editor') {
+} else if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'dataBeritaNaskah_editor') {
     include 'views/pages/editor/dataBeritaNaskah_editor.php';
-}else {
+} else if (isset($_GET['t_editor']) && $_GET['t_editor'] == 'buatpaket_editor') {
+    include 'views/pages/editor/buatpaket_editor.php';
+} else {
     include 'views/pages/editor/beranda_editor.php';
 }
 
--- views/layout/sidebar.php
+++ views/layout/sidebar.php
@@ -261,6 +261,13 @@
                                       <i class="far fa-circle nav-icon"></i>
                                       <p>Data Lead</p>
                                   </a>
+                              </li>
+                              <li class="nav-item">
+                                  <a href="<?= $base_url ?>buatpaket_editor" class="nav-link">
+                                      <i class="far fa-circle nav-icon"></i>
+                                      <p>Data Paket</p>
+                                  </a>
+                              </li>
                           </ul>
                       </li>
                   </ul>
