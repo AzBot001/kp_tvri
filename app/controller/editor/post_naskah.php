57b16cf653c5ff399d6e09a364b8cfb1e40dac95
AbidHarun <abidmuhaimin15@gmail.com>
2022-07-31 21:49:30 +0800

    memperbaiki post durasi

--- app/controller/editor/post_naskah.php
+++ app/controller/editor/post_naskah.php
@@ -7,7 +7,7 @@ if (isset($_POST['durasi'])) {
     $id = $_POST['id'];
   $m = $_POST['m'];
   $s = $_POST['s'];
-  $durasi = $m.':'.$s;
+  $durasi = '00:'.$m.':'.$s;
 
   $mysqli->query("UPDATE naskah SET durasi = '$durasi', sts_edit = '1' WHERE id_naskah = '$id'");
   flash('durasighi','Berhasil menambahkan durasi');
